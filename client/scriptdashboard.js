bfe05cbddfce4511549c8ba8a0d98227e36cb6a6
Marlon <marlonmjr16@gmail.com>
2026-04-16 20:47:52 -0300

    feito 2

--- client/scriptdashboard.js
+++ client/scriptdashboard.js
@@ -2,5 +2,20 @@ const burgermenu = document.querySelector("#burgermenu");
 const navlinks = document.querySelector("#navlinks");
 
 burgermenu.addEventListener("click", () => {
+    navlinks.classList.toggle("hidden");
     navlinks.classList.toggle("opacity-100");
+});
+
+const logoutButtons = document.querySelectorAll("#logoutButton");
+
+logoutButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let confirmar = confirm("Tem certeza de que deseja sair?");
+    
+    if (!confirmar) {
+      alert("Logout cancelado.");
+    } else {
+      window.location.href = "login.html";
+    }
+  });
 });
