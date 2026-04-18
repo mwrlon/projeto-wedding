430db7c6b81d15fe47e91009052f9a237b98885c
Marlon <marlonmjr16@gmail.com>
2026-04-17 21:51:37 -0300

    salvando

--- client/scriptconvidados.js
+++ client/scriptconvidados.js
@@ -1,12 +1,14 @@
 const burgermenu = document.querySelector("#burgermenu");
 const navlinks = document.querySelector("#navlinks");
+const logoutButtons = document.querySelectorAll("#logoutButton");
+const fechar = document.querySelector("#fechar");
+const popupCadastro = document.querySelector("#popupCadastro");
+const background = document.querySelector("#background");
 
 burgermenu.addEventListener("click", () => {
     navlinks.classList.toggle("hidden");
     navlinks.classList.toggle("opacity-100");
 });
-
-const logoutButtons = document.querySelectorAll("#logoutButton");
 
 logoutButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -22,4 +24,17 @@ logoutButtons.forEach((btn) => {
 
 const cadastrarNovo = () => {
     popupCadastro.classList.remove("hidden");
-}
+      background.classList.add("blur-sm");
+      background.classList.add("pointer-events-none");
+      header.classList.add("blur-sm");
+      header.classList.add("pointer-events-none");
+
+};
+
+fechar.addEventListener("click", () => {
+    popupCadastro.classList.toggle("hidden");
+    background.classList.remove("blur-sm");
+    background.classList.remove("pointer-events-none");
+    header.classList.remove("blur-sm");
+    header.classList.remove("pointer-events-none");
+});
